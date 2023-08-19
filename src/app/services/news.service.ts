4589198012e8c752cebbc6002f0591d02b2396b5
Inaamul shaikh <inamulshaikh10@gmail.com>
2023-08-19 12:32:28 +0530

    Final commit

---
+++ src/app/services/news.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  api_key = '419c594c9e3a40a3ad36d5802d19fe79';
+  constructor(private http: HttpClient) { }
+
+
+  initSources() {
+    return this.http.get('https://newsapi.org/v2/sources?language=en&apiKey=' + this.api_key);
+  }
+
+
+  initArticles() {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=' + this.api_key);
+  }
+
+  getArticlesByID(source: String) {
+    return this.http.get('https://newsapi.org/v2/top-headlines?sources=' + source + '&apiKey=' + this.api_key);
+  }
+
+}
